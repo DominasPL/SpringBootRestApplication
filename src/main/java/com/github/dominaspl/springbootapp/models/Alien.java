3a1546b26c960c3a84e54093711370bd3ab9db80
DomiasPL <dominik-stepuch@wp.pl>
2019-08-20 16:57:54 +0200

    Added database config to application properties file

--- src/main/java/com/github/dominaspl/springbootapp/models/Alien.java
+++ src/main/java/com/github/dominaspl/springbootapp/models/Alien.java
@@ -3,10 +3,16 @@ package com.github.dominaspl.springbootapp.models;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "aliens")
 @Data
 @RequiredArgsConstructor
 public class Alien {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
